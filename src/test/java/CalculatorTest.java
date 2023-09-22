f35b5fdd0dd6d3a70fb5179a407b55b80b0c711c
Janani Ayeshika <jananiayeshika1@gmail.com>
2023-09-22 21:05:45 +0800

    Initial Commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    private  Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        Assertions.assertEquals(10, calculator.add(3,7));
+    }
+
+    @Test
+    public void testSubstract() {
+        Assertions.assertEquals(7, calculator.subtract(14,7));
+    }
+
+    @Test
+    public void testMultiply() {
+        Assertions.assertEquals(98, calculator.multiply(14,7));
+    }
+
+    @Test
+    public void testdivide() {
+        Assertions.assertEquals(2, calculator.divide(14,7));
+    }
+}
